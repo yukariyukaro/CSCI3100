51dc7e8ccf29a6855f9ce7e0ff87f6d2487b9484
LI Peijin <1155211221@link.cuhk.edu.hk>
2026-04-11 02:12:32 +0800

    fix: resolve sidekiq config error and isolate redis in cucumber

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,16 +1,15 @@
 Sidekiq.configure_server do |config|
-  config.redis = if Rails.env.test?
-                   { url: "redis://localhost:6379", connection_pool: nil }
-                 else
-                   { url: ENV.fetch("REDIS_URL", "redis://localhost:6379/1") }
-                 end
+  if Rails.env.test?
+    config.redis = { url: 'redis://localhost:6379' }
+  else
+    config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
+  end
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = if Rails.env.test?
-                   # In test mode, we don't want Sidekiq to actually try connecting to Redis
-                   { url: "redis://localhost:6379", connection_pool: nil }
-                 else
-                   { url: ENV.fetch("REDIS_URL", "redis://localhost:6379/1") }
-                 end
+  if Rails.env.test?
+    config.redis = { url: 'redis://localhost:6379' }
+  else
+    config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
+  end
 end
