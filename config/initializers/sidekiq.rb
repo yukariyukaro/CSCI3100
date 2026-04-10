204a628a387cb27b4aa2f61befac342ea13a2f7f
LI Peijin <1155211221@link.cuhk.edu.hk>
2026-04-11 02:13:09 +0800

    fix: remove illegal connection_pool key and force inline sidekiq for test

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,15 +1,15 @@
 Sidekiq.configure_server do |config|
-  if Rails.env.test?
-    config.redis = { url: 'redis://localhost:6379' }
-  else
-    config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
-  end
+  config.redis = if Rails.env.test?
+                   { url: "redis://localhost:6379" }
+                 else
+                   { url: ENV.fetch("REDIS_URL", "redis://localhost:6379/1") }
+                 end
 end
 
 Sidekiq.configure_client do |config|
-  if Rails.env.test?
-    config.redis = { url: 'redis://localhost:6379' }
-  else
-    config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
-  end
+  config.redis = if Rails.env.test?
+                   { url: "redis://localhost:6379" }
+                 else
+                   { url: ENV.fetch("REDIS_URL", "redis://localhost:6379/1") }
+                 end
 end
