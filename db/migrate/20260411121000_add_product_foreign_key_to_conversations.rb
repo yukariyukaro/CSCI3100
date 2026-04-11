5034346595d40eb6c6480e57f077d360100d5ea7
sty0000 <1155211107@link.cuhk.edu.hk>
2026-04-12 00:44:02 +0800

    fix(db): make FK migration idempotent and align specs

--- db/migrate/20260411121000_add_product_foreign_key_to_conversations.rb
+++ db/migrate/20260411121000_add_product_foreign_key_to_conversations.rb
@@ -1,5 +1,13 @@
 class AddProductForeignKeyToConversations < ActiveRecord::Migration[7.1]
-  def change
-    add_foreign_key :conversations, :products
+  def up
+    return if foreign_key_exists?(:conversations, :products, column: :product_id)
+
+    add_foreign_key :conversations, :products, column: :product_id
+  end
+
+  def down
+    return unless foreign_key_exists?(:conversations, :products, column: :product_id)
+
+    remove_foreign_key :conversations, column: :product_id
   end
 end
